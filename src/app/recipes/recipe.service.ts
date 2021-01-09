6859ed4de0cd2869a8f145bb991819c2776bee7f
Kamal Singh <kamal.yadav@signzy.com>
2021-01-09 19:33:34 +0530

    all might push

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,18 +1,20 @@
 import { Recipe } from './recipe.model';
-import { EventEmitter, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
 @Injectable()
-export class RecipeService {
-    recipeSelected = new EventEmitter<Recipe>();
-    
+export class RecipeService {    
     private recipes: Recipe[] = [
         new Recipe('name','description','https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/chorizo-mozarella-gnocchi-bake-cropped.jpg', [new Ingredient('Meat', 1)])
       ];
 
     getRecipes() {
         return this.recipes.slice();
+    }
+
+    getRecipe(index: number) {
+        return this.recipes[index];
     }
     constructor(private slService: ShoppingListService) {}
 
